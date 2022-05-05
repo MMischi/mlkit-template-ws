c76b5230842b0a43f02cbe2de3f5a64304d58d62
ComradeVanti <kingvanti650@gmail.com>
2022-05-05 10:38:12 +0200

    Add logic to take and display picture

--- app/src/main/java/com/example/mlkit_ws/Sample_1.kt
+++ app/src/main/java/com/example/mlkit_ws/Sample_1.kt
@@ -1,59 +1,86 @@
 package com.example.mlkit_ws
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.fragment_sample_1.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Sample_1.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Sample_1 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val REQUEST_CAMERA = 123
+    private val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA)
+
+    private lateinit var originalTextView: TextView
+    private lateinit var translatedTextView: TextView
+    private lateinit var picImageView: ImageView
+
+    private lateinit var checkPermissions: ActivityResultLauncher<String>
+    private lateinit var launchCam: ActivityResultLauncher<Intent>
+
+
+    private val allPermissionsGranted
+        get() =
+            REQUIRED_PERMISSIONS.all {
+                ContextCompat.checkSelfPermission(
+                    requireContext(), it
+                ) == PackageManager.PERMISSION_GRANTED
+            }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sample_1, container, false)
+    ): View? =
+        inflater.inflate(R.layout.fragment_sample_1, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        originalTextView = view.findViewById(R.id.txt_original)
+        translatedTextView = view.findViewById(R.id.txt_translated)
+        picImageView = view.findViewById(R.id.img_pic)
+        view.findViewById<Button>(R.id.btn_translate).setOnClickListener { onTranslateClicked() }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Sample_1.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Sample_1().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        checkPermissions =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+                if (!granted)
+                    Toast.makeText(context, "Bruh", Toast.LENGTH_SHORT).show()
             }
+
+        launchCam = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            val img = it.data?.extras?.get("data") as Bitmap?
+            if (img != null)
+                picImageView.setImageBitmap(img)
+            else
+                Toast.makeText(requireContext(), "Very sad, no picture", Toast.LENGTH_SHORT).show()
+        }
+
+        checkPermissions.launch(Manifest.permission.CAMERA)
     }
+
+    public fun onTranslateClicked() {
+        if (allPermissionsGranted) {
+            launchCam.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
+        }
+    }
+
 }
